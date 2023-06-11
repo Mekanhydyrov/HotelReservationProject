992d06ae5344ab56620c6f112450c55c6f0f4959
Mekan Hydyrov <78173532+Mekanhydyrov@users.noreply.github.com>
2023-06-11 19:48:16 +0300

    Katmanların Oluşturulması Tamamlandı

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfTestimonialDal.cs
@@ -0,0 +1,21 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.EntityFramework
+{
+    public class EfTestimonialDal : IGenericRepository<Testimonial>, ITestimonialDal
+    {
+        public EfTestimonialDal(Context context) : base(context) 
+        {
+            
+        }
+    }
+}
